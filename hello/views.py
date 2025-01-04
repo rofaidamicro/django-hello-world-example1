12a9bbbf794825e180c39f6d35994a68b5446a16
rofaidamicro <nezzar.rofaida.24@gmail.com>
2025-01-05 01:08:21 +0200

    Add Example 2: Improved Django page with MVT pattern

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def hello_world(request):
-    return render(request, 'hello/hello.html')
+    # Dynamic data passed to the template
+    context = {
+        'title': 'Hello, World!',
+        'message': 'Welcome to the improved Django page created with the MVT pattern!',
+        'button_text': 'Click Me!',
+    }
+    return render(request, 'hello/hello.html', context)
